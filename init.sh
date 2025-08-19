d82d67f91ca34df4f5d71fadc6d1bfe3cb4f7fcd
wobbat <mail@wobbat.com>
2025-08-19 20:35:55 +0200

    init

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo pacman -Sy unzip
 curl -L https://github.com/oven-sh/bun/releases/latest/download/bun-linux-x64.zip -o /tmp/bun.zip
 unzip /tmp/bun.zip -d /tmp/bun
-/tmp/bun/bun "$(pwd)/index.ts"
+/tmp/bun/bun-linux-x64/bun "$(pwd)/index.ts"
 
